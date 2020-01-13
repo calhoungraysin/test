66b83eb27df425ef8cd2fe70625ddb06dac3f741
Maxim Shiryaev <maksim155555@gmail.com>
2020-01-13 21:06:48 +0200

    add space

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import requests
 city = input()
 app_key = '9da81b09ab671d99202764fe681d24a6'
 
-responce = requests.get(f'https://api.openweathermap.org/data/2.5/forecast/daily?q={city}&cnt=3&appid={app_key}')
+responce = requests.get(f'https://api.openweathermap.org/data/2.5/forecast?q={city}&cnt=3&appid={app_key}')
 
 responce = responce.json()
+
 
 print(pprint.pprint(responce))
