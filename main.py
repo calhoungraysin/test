d8ff8a8aaf7be4341ac46ebc8fad0699a799c71f
Maxim Shiryaev <maksim155555@gmail.com>
2020-01-13 21:02:29 +0200

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+import pprint
+
+import requests
+
+city = input()
+app_key = '9da81b09ab671d99202764fe681d24a6'
+
+responce = requests.get(f'https://api.openweathermap.org/data/2.5/forecast/daily?q={city}&cnt=3&appid={app_key}')
+
+responce = responce.json()
+
+print(pprint.pprint(responce))
